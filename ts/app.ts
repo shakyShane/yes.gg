ec9029c3aaf74f49d9bf651512001a1710a64d4a
Shane Osbourne <shane.osbourne8@gmail.com>
2016-07-02 15:43:16 +0100

    Added Hero block to homepage

--- ts/app.ts
+++ ts/app.ts
@@ -2,9 +2,11 @@
 
 import nav from './nav';
 import overlays from './overlays';
+import heros from './heros';
 
 console.log('app.ts loaded');
 
 nav();
 overlays();
+heros();
 
---
+++ ts/heros.ts
@@ -0,0 +1,82 @@
+require('zepto/zepto.min.js');
+const $ = window['Zepto'];
+import state from './state';
+// const debug = require('debug')('sd:heros.ts');
+
+export const HeroAttributes = {
+    loadingClass: 'hero--loading',
+    srcAttr: <any>'hero-src',
+    largeAttr: <any>'hero-src-large',
+};
+
+export interface HeroItem {
+    $elem: any,
+    src: any,
+    large: string,
+    current: string
+}
+
+export default function init () {
+
+    /**
+     * Gather hero items on page load
+     * @type {any}
+     */
+    const heros = $.map($('[hero-src]'), function (item): HeroItem {
+        const $elem = $(item);
+        const src = $elem.attr(HeroAttributes.srcAttr);
+        const large = $elem.attr(HeroAttributes.largeAttr);
+        return {
+            $elem,
+            src: src,
+            large: large,
+            current: ''
+        }
+    });
+
+    /**
+     * If any hero items exist on page load
+     */
+    if (heros.length) {
+        loadFromCollection(heros);
+        $(window).resize(function () {
+            loadFromCollection(heros);
+        });
+    }
+
+    /**
+     * Run the initial load
+     */
+    function loadFromCollection (collection) {
+        if (!collection.length) return;
+        load(collection[0], function () {
+            collection.slice(1).forEach(load);
+        });
+    }
+
+    function load(item, cb) {
+        cb = cb || function() {};
+
+        const selection = (function () {
+            if (state.aboveLap) return item.large;
+            return item.src;
+        })();
+
+        if (item.current && item.current === selection) {
+            // debug(`BAIL ${item.current}`);
+            return cb();
+        }
+
+        const img = $(`<img src="${selection}"/>`);
+
+
+        img[0].onload = function () {
+            console.log('added');
+            img.remove();
+            item.current = selection;
+            item.$elem.parent().removeClass(HeroAttributes.loadingClass);
+            item.$elem.css('background-image', `url(${selection})`);
+            cb();
+        };
+    }
+}
