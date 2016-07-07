11453ecf7d801b75eb33d3ca9c89fce0e0e6a8cb
Shane Osbourne <shane.osbourne8@gmail.com>
2016-07-07 22:22:40 +0100

    Adding photoswipe

--- ts/app.ts
+++ ts/app.ts
@@ -5,12 +5,14 @@ import overlays from './overlays';
 import heros from './heros';
 import buttons from './buttons';
 import testimonials from './testimonials';
+import zoomer from './zoomer';
 
 nav();
-
 overlays();
 heros();
 buttons();
-
-
 testimonials();
+
+
+
+zoomer();
---
+++ ts/zoomer.ts
@@ -0,0 +1,54 @@
+require('zepto/zepto.min.js');
+const $ = window['Zepto'];
+
+const PhotoSwipe = require('photoswipe');
+const PhotoswipeUIDefault = require('photoswipe/dist/photoswipe-ui-default');
+
+export default function () {
+    const $linkItems = $('.gallery__link');
+
+    if (!$linkItems.length) return;
+
+    const items = $.map($('.gallery__link'), function (elem) {
+        return {
+            src: $(elem).attr('href'),
+            h: Number($(elem).attr('data-height')),
+            w: Number($(elem).attr('data-width'))
+        }
+    });
+
+    $('.gallery').on('click', '.gallery__link', function (evt) {
+        evt.preventDefault();
+        const index = $linkItems.indexOf(this);
+        initGallery(items, index, this);
+        console.log(items);
+    });
+}
+
+function initGallery (items, index, elem) {
+    const gallery = new PhotoSwipe(
+        document.querySelector('.pswp'),
+        PhotoswipeUIDefault,
+        items,
+        {
+            index: index,
+            showHideOpacity: true,
+            getThumbBoundsFn: function(index) {
+
+                // find thumbnail element
+
+                // get window scroll Y
+                var pageYScroll = window.pageYOffset || document.documentElement.scrollTop;
+                // optionally get horizontal scroll
+
+                // get position of element relative to viewport
+                var rect = elem.getBoundingClientRect();
+
+                // w = width
+                return {x:rect.left, y:rect.top + pageYScroll, w:rect.width};
+            }
+        }
+    );
+    gallery.init();
+}
+
