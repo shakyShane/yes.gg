c3ce4d1d238fcb78d908e230248585cc08e1c267
Shane Osbourne <shane.osbourne8@gmail.com>
2016-07-10 14:37:57 +0100

    Breadcrumbs

--- ts/app.ts
+++ ts/app.ts
@@ -6,13 +6,14 @@ import heros from './heros';
 import buttons from './buttons';
 import testimonials from './testimonials';
 import zoomer from './zoomer';
+const scroll = require('smooth-scroll');
+
+scroll.init();
 
 nav();
 overlays();
 heros();
 buttons();
 testimonials();
-
-
 
 zoomer();
